591697bcd0104027be284bb10e8e6aabbe56927a
gkalvarog <g.kalvaro.ag@gmail.com>
2024-10-21 18:52:29 +0300

    Update/edited the bank app to match some other requirements. OpenAI helped me reorganize the structure

--- dio-bank.py
+++ dio-bank.py
@@ -1,89 +1,139 @@
-'''
-Requirements:
+import datetime, pytz
 
-1. Deposit: only positive amounts, added to one variable and shown on the statement (assign a variable)
-2. Withdrawal: up to 3 daily withdrawals, each of 500. If not enough balance, pop up message (not enough balance), added to one variable and shown on the statement (assign a variable)
-3. Statement: display balance as a format "R$ {balance}"
-'''
+bank_log = []
+clients = []
+current_customer = None
+LIMIT_DAILY_TRANSACTIONS = 10
 
 menu = """
+Menu
 [d] Deposit
 [w] Withdraw
 [s] Statement
+[n] New account
+[l] Login
+[o] Logout
 [e] Exit
 => """
 
-balance = 0
-limit = 500
-statement = []
-max_amount_withdrawal = balance
-number_withdrawals = 0
-LIMIT_WITHDRAWAL = 3
+class Client:
+    def __init__(self, name, taxid):
+        self.name = name
+        self.taxid = taxid
+        self.transactions = [] 
+        self.balance = 0
+        self.transactions_made = 0
 
+    def make_transaction(self, transaction):
+        if self.transactions_made >= LIMIT_DAILY_TRANSACTIONS:
+            print("You have exceeded the number of transactions allowed for today!")
+            return False
+        self.transactions.append(transaction)
+        self.transactions_made += 1
+        return True
+
+    def deposit(self, amt):
+        now = datetime.datetime.now(pytz.utc).strftime("%Y-%m-%d, %H:%M:%S, UTC")
+        if self.make_transaction(Transaction("d", amt, now)):
+            self.balance += amt
+            bank_log.append(f"Deposit: {amt} on {now}, New Balance: {self.balance}")
+            print(f"Deposit successful. New Balance: ${self.balance}")
+        else:
+            print("Transaction failed.")
+
+    def withdraw(self, amt):
+        now = datetime.datetime.now(pytz.utc).strftime("%Y-%m-%d, %H:%M:%S, UTC")
+        if self.balance >= amt:
+            if self.make_transaction(Transaction("w", amt, now)):
+                self.balance -= amt
+                bank_log.append(f"Withdrawal: {amt} on {now}, New Balance: {self.balance}")
+                print(f"Withdrawal successful. New Balance: ${self.balance}")
+            else:
+                print("Transaction failed.")
+        else:
+            print(f"Insufficient funds. You can withdraw up to ${self.balance}.")
+
+    def show_statement(self):
+        print(f"Balance: ${self.balance}")
+        if self.transactions:
+            print("Transaction History:")
+            for transaction in self.transactions:
+                print(f"Type: {transaction.type}, Amount: ${transaction.amount}, Date: {transaction.date}")
+        else:
+            print("No transactions to show.")
+
+class Transaction:
+    def __init__(self, type, amount, date):
+        self.type = type
+        self.amount = amount
+        self.date = date
+
+def create_new_account(name, taxid):
+    clients.append(Client(name, taxid))
+    now = datetime.datetime.now(pytz.utc).strftime("%Y-%m-%d, %H:%M:%S, UTC")
+    bank_log.append(f"Account Creation: Name: {name}, Tax ID: {taxid}, Time: {now}")
+    print(f"Account created successfully.")
+
+def set_current_user(taxid):
+    result = list(filter(lambda p: p.taxid == taxid, clients))
+    if result:
+        return result[0]
+    else:
+        print("Account not found.")
+        return None
+
+def want_to_continue():
+    return input("Would you like to make another operation? (y/n)\n").lower() == "y"
+
+def print_final_log():
+    print("\n--- Session Log ---")
+    for log_entry in bank_log:
+        print(log_entry)
+    print("\n--- End of Session ---")
 
 while True:
-
     choice = input(menu)
 
-    if choice == "d":
-        deposit = abs(int(input("How much you desire to deposit? Write the amount in number notation, ex: 1000.\n")))
-        balance += deposit
-        statement.append(deposit)
+    if choice == "n":
+        taxid = input("Enter your Tax ID:\n")
+        name = input("Enter your name:\n")
+        create_new_account(name, taxid)
+        current_customer = set_current_user(taxid)
 
-        # updating the maxmim amount of withdrawal on a given day, as we cannot say that 2000 can be withdrawn even if there are 1500+ in the account.
-        if balance > 1500:
-            max_amount_withdrawal = 1500
-
-        # ask if wants to continue making transactions
-        new_op = input(f"Your balance is R$ {balance}. Would you like to make a new operation?\n")
-        
-        if new_op[0].lower() == "n":
-            break
-
-    elif choice == "w":
-        if number_withdrawals < LIMIT_WITHDRAWAL:
-
-            withdrawal = abs(int(input("How much you desire to withdraw? Write the amount in number notation, ex: 500.\n")))
-
-            if balance >= withdrawal:
-
-                if withdrawal <= 500:
-                    
-                    balance -= withdrawal
-                    statement.append(-withdrawal)
-
-                    number_withdrawals += 1
-
-                    # updating the maxmim amount of withdrawal on a given day, as we cannot say that 1200 can be withdrawn if 1 withdrawal has already happened.
-                    max_amount_withdrawal = (LIMIT_WITHDRAWAL - number_withdrawals) * 500
-
-                    # ask if wants to continue making transactions
-                    new_op = input(f"Your balance is R$ {balance}. Would you like to make a new operation?\n")
-                    
-                    if new_op[0].lower() == "n":
-                        break
-                
-                else:
-                    print("You can withdraw up to R$ 500 per time. Repeat the operation.")
-
-            else:
-                print(f"There is not enough balance to complete this transaction. At this moment, you can withdraw up to R$ {max_amount_withdrawal}. Repeat the operation.")
-
+    elif choice == "l":
+        if current_customer:
+            print("You are already logged in. Log out first if you want to switch accounts.")
         else:
-            print(f"You can withdraw up to {LIMIT_WITHDRAWAL} times per day. Try again tomorrow.")
-            break
+            taxid = input("Enter your Tax ID to login:\n")
+            current_customer = set_current_user(taxid)
+            if current_customer:
+                print(f"Logged in as {current_customer.name}")
 
-    elif choice == "s":
-        print(f"Your balance is R$ {balance}. This is the history of your transactions: {statement}.")
-        
-        # ask if wants to continue making transactions
-        new_op = input(f"Would you like to make a new operation?\n")
-                    
-        if new_op[0].lower() == "n":
-            break
+    elif choice == "o":
+        if current_customer:
+            print(f"Logging out of {current_customer.name}'s account.")
+            current_customer = None
+        else:
+            print("No account is currently logged in.")
+
+    elif choice == "d" and current_customer:
+        dep = abs(int(input("How much do you want to deposit? (e.g., 1000)\n")))
+        current_customer.deposit(dep)
+
+    elif choice == "w" and current_customer:
+        withdrawal = abs(int(input("How much do you want to withdraw? (e.g., 500)\n")))
+        current_customer.withdraw(withdrawal)
+
+    elif choice == "s" and current_customer:
+        current_customer.show_statement()
 
     elif choice == "e":
+        print_final_log()
         break
 
     else:
-        print("Invalid operation. Please select a valid operation.")
+        print("Invalid operation or no account logged in. Please select a valid option.")
+    
+    if current_customer and not want_to_continue():
+        print_final_log()
+        break
